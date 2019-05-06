9630e7fa5a606275b5aaee39cc8d6a36c700af23
danielcmcg <49722310+danielcmcg@users.noreply.github.com>
2019-05-06 13:59:28 -0300

    adding scripts UIDrag and UIDrop
    adding scripts UIDrag and UIDrop under Scripts folder

---
+++ Scripts/UIDrag.cs
@@ -0,0 +1,53 @@
+﻿/*
+For a thanks, go to the GitHub original project: https://github.com/danielcmcg/Unity-UI-Nested-Drag-and-Drop 
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIDrag : MonoBehaviour, IDragHandler, IEndDragHandler, IPointerDownHandler
+{
+    Vector3 startPosition;
+    Vector3 diffPosition;
+    GameObject canvas_;
+    static bool updateSize;
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition - diffPosition;
+        //Debug.Log(Input.mousePosition);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        updateSize = true;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        startPosition = transform.position;
+        diffPosition = Input.mousePosition - startPosition;
+        EventSystem.current.SetSelectedGameObject(gameObject);
+        EventSystem.current.currentSelectedGameObject.transform.SetParent(canvas_.transform);
+        EventSystem.current.currentSelectedGameObject.transform.SetAsFirstSibling();
+        Debug.Log("start drag " + gameObject.name);
+    }
+
+    void Start()
+    {
+        canvas_ = GameObject.Find("Canvas");
+        updateSize = false;
+    }
+
+    void Update()
+    {
+        if(updateSize == true)
+        {
+            LayoutRebuilder.ForceRebuildLayoutImmediate(GetComponent<RectTransform>());
+        }
+    }
+
+}
---
+++ Scripts/UIDrop.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIDrop : MonoBehaviour, IDropHandler
+{
+    
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(EventSystem.current.currentSelectedGameObject);
+        
+        EventSystem.current.currentSelectedGameObject.transform.parent = transform;
+        var sizey = GetComponent<RectTransform>().rect.size.y;
+        int index = (int)((((transform.position.y+(sizey/2)) - Input.mousePosition.y))/35)+1;
+        if(index <= 0)
+        {
+            index = 1;
+        }
+        Debug.Log(index);
+        EventSystem.current.currentSelectedGameObject.transform.SetSiblingIndex(index);
+        //transform.parent
+
+        Debug.Log("drop " + gameObject.name);
+    }
+
+}
